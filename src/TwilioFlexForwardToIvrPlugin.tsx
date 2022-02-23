90d2740a40f46add7a42d48da41d410bdbebccec
Bruno Kilian <bkilian@twilio.com>
2022-02-23 16:35:49 +0100

    add: UI

--- src/TwilioFlexForwardToIvrPlugin.tsx
+++ src/TwilioFlexForwardToIvrPlugin.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import * as Flex from '@twilio/flex-ui';
 import { FlexPlugin } from 'flex-plugin';
-
-import CustomTaskListContainer from './components/CustomTaskList/CustomTaskList.Container';
-import reducers, { namespace } from './states';
+import { Button, Tab } from '@twilio/flex-ui';
 
 const PLUGIN_NAME = 'TwilioFlexForwardToIvrPlugin';
 
@@ -20,27 +18,49 @@ export default class TwilioFlexForwardToIvrPlugin extends FlexPlugin {
    * @param manager { Flex.Manager }
    */
   init(flex: typeof Flex, manager: Flex.Manager) {
-    this.registerReducers(manager);
-
-    const options: Flex.ContentFragmentProps = { sortOrder: -1 };
-    flex.AgentDesktopView
-      .Panel1
-      .Content
-      .add(<CustomTaskListContainer key="TwilioFlexForwardToIvrPlugin-component" />, options);
-  }
-
-  /**
-   * Registers the plugin reducers
-   *
-   * @param manager { Flex.Manager }
-   */
-  private registerReducers(manager: Flex.Manager) {
-    if (!manager.store.addReducer) {
-      // tslint: disable-next-line
-      console.error(`You need FlexUI > 1.9.0 to use built-in redux; you are currently on ${Flex.VERSION}`);
-      return;
-    }
-
-    manager.store.addReducer(namespace, reducers);
+    flex.TaskCanvasTabs.Content.add(<ForwardToIVR key="forward-to-ivr" label="Forward to IVR" uniqueName="forward-to-ivr" />, {
+      if: ({ task: { channelType } }: any) => {
+        return channelType === 'voice';
+      },
+    });
   }
 }
+
+interface Props {
+  label: string;
+  uniqueName: string;
+  key: string;
+  task?: {
+    attributes: string;
+  };
+}
+const ForwardToIVR: React.FC<Props> = (props) => {
+  const { task } = props;
+  const { attributes } = task!;
+
+  console.log('@@@task', task);
+  console.log('@@@taskattributes', attributes);
+  // console.log('@@@props', props);
+  const styles: any = {
+    div: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      padding: '6px 12px',
+      flex: 1,
+    },
+    button: {
+      fontSize: '20px',
+      margin: '15px',
+    },
+  };
+  return (
+    <Tab uniqueName="forward-to-ivr" key="forward-to-ivr">
+      <div style={styles.div}>
+        <Button style={styles.button}>Main menu</Button>
+        <Button style={styles.button}>Sales menu</Button>
+        <Button style={styles.button}>Service menu</Button>
+      </div>
+    </Tab>
+  );
+};
