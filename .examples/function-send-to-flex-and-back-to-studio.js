df948759c7d296c5622336e9b4e8730dee22e54d
Bruno Kilian <bkilian@twilio.com>
2022-02-23 22:43:31 +0100

    add examples

---
+++ .examples/function-send-to-flex-and-back-to-studio.js
@@ -0,0 +1,62 @@
+// Get the SID of your Studio Flow on this link, it starts with "FW": https://console.twilio.com/us1/develop/studio/flows?frameUrl=/console/studio/flows
+const studioFlowSid = 'FWxxxx';
+
+// Get your taskRouterWorkflow SID on this link, it starts with "WW": https://console.twilio.com/us1/develop/taskrouter/workspaces?frameUrl=/console/taskrouter/workspaces
+const taskRouterWorkflow = 'WWxxxx';
+
+const Response = (callback, xml) => {
+  const response = new Twilio.Response();
+  response.appendHeader('Content-Type', 'application/xml');
+  response.setBody(xml.trim());
+  response.appendHeader('Access-Control-Allow-Origin', '*');
+  response.appendHeader('Access-Control-Allow-Methods', 'OPTIONS POST GET');
+  response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
+  callback(null, response);
+  console.log('response: ', xml.trim());
+};
+exports.handler = async (context, event, callback) => {
+  console.log('event', event);
+  console.log('context', context);
+
+  const { ACCOUNT_SID, PATH, DOMAIN_NAME } = context;
+  const { CallSid, forwardToIVRMenu } = event;
+
+  //
+  // Request came from Studio Flow to forward the call to Flex
+  //
+  if (!forwardToIVRMenu) {
+    console.log('Step 1 - Sending call to Flex...');
+
+    const url = `https://${DOMAIN_NAME}${PATH}`;
+
+    return Response(
+      callback,
+      `
+        <?xml version="1.0" encoding="UTF-8"?>
+        <Response>
+            <Enqueue workflowSid="${taskRouterWorkflow}">
+                <Task>{"forwardToIvrUrl": "${url}"}</Task>
+            </Enqueue>     
+        </Response>
+    `
+    );
+  }
+
+  //
+  // Request came from Flex to forward the call back to Studio
+  //
+  console.log('Step 2 - Sending call from Flex back to Studio...');
+
+  const twiml = `
+      <?xml version="1.0" encoding="UTF-8"?>
+      <Response>
+          <Redirect>https://webhooks.twilio.com/v1/Accounts/${ACCOUNT_SID}/Flows/${studioFlowSid}?FlowEvent=return&amp;forwardToIVRMenu=${forwardToIVRMenu}</Redirect>
+      </Response>
+  `.trim();
+
+  console.log('CallSid', CallSid);
+  console.log('twiml', twiml);
+  const client = context.getTwilioClient();
+  await client.calls(CallSid).update({ twiml });
+  return Response(callback, 'ok');
+};
---
+++ .examples/studio-flow-json.js
@@ -0,0 +1,234 @@
+{
+    "description": "Demo Forward to IVR",
+    "states": [
+      {
+        "name": "Trigger",
+        "type": "trigger",
+        "transitions": [
+          {
+            "event": "incomingMessage"
+          },
+          {
+            "next": "mainMenu",
+            "event": "incomingCall"
+          },
+          {
+            "event": "incomingRequest"
+          },
+          {
+            "event": "incomingParent"
+          }
+        ],
+        "properties": {
+          "offset": {
+            "x": 0,
+            "y": 0
+          }
+        }
+      },
+      {
+        "name": "sendToFlex",
+        "type": "add-twiml-redirect",
+        "transitions": [
+          {
+            "next": "backToIVRSplit",
+            "event": "return"
+          },
+          {
+            "event": "timeout"
+          },
+          {
+            "event": "fail"
+          }
+        ],
+        "properties": {
+          "offset": {
+            "x": 270,
+            "y": 1220
+          },
+          "method": "POST",
+          "url": "https://manual-tests-bruno-1234.twil.io/send-to-flex-and-back-to-studio",
+          "timeout": "14400"
+        }
+      },
+      {
+        "name": "mainMenu",
+        "type": "gather-input-on-call",
+        "transitions": [
+          {
+            "next": "mainMenuSplit",
+            "event": "keypress"
+          },
+          {
+            "event": "speech"
+          },
+          {
+            "event": "timeout"
+          }
+        ],
+        "properties": {
+          "voice": "Polly.Joanna",
+          "number_of_digits": 1,
+          "speech_timeout": "auto",
+          "offset": {
+            "x": 150,
+            "y": 240
+          },
+          "loop": 1,
+          "finish_on_key": "#",
+          "say": "Welcome to Twilio! Press 1 to Sales or 2 to Services",
+          "language": "en-US",
+          "stop_gather": true,
+          "gather_language": "en-US",
+          "profanity_filter": "true",
+          "timeout": 5
+        }
+      },
+      {
+        "name": "mainMenuSplit",
+        "type": "split-based-on",
+        "transitions": [
+          {
+            "event": "noMatch"
+          },
+          {
+            "next": "saySales",
+            "event": "match",
+            "conditions": [
+              {
+                "friendly_name": "sales",
+                "arguments": [
+                  "{{widgets.mainMenu.Digits}}"
+                ],
+                "type": "equal_to",
+                "value": "1"
+              }
+            ]
+          },
+          {
+            "next": "sayServices",
+            "event": "match",
+            "conditions": [
+              {
+                "friendly_name": "services",
+                "arguments": [
+                  "{{widgets.mainMenu.Digits}}"
+                ],
+                "type": "equal_to",
+                "value": "2"
+              }
+            ]
+          }
+        ],
+        "properties": {
+          "input": "{{widgets.mainMenu.Digits}}",
+          "offset": {
+            "x": -210,
+            "y": 510
+          }
+        }
+      },
+      {
+        "name": "sayServices",
+        "type": "say-play",
+        "transitions": [
+          {
+            "next": "sendToFlex",
+            "event": "audioComplete"
+          }
+        ],
+        "properties": {
+          "voice": "Polly.Joanna",
+          "offset": {
+            "x": 420,
+            "y": 930
+          },
+          "loop": 1,
+          "say": "Welcome to Services, please wait while I forward your call to one of your specialists.",
+          "language": "en-US"
+        }
+      },
+      {
+        "name": "saySales",
+        "type": "say-play",
+        "transitions": [
+          {
+            "next": "sendToFlex",
+            "event": "audioComplete"
+          }
+        ],
+        "properties": {
+          "voice": "Polly.Joanna",
+          "offset": {
+            "x": 70,
+            "y": 930
+          },
+          "loop": 1,
+          "say": "Welcome to Sales, please wait while I forward your call to one of your specialists.",
+          "language": "en-US"
+        }
+      },
+      {
+        "name": "backToIVRSplit",
+        "type": "split-based-on",
+        "transitions": [
+          {
+            "event": "noMatch"
+          },
+          {
+            "next": "saySales",
+            "event": "match",
+            "conditions": [
+              {
+                "friendly_name": "sales",
+                "arguments": [
+                  "{{widgets.sendToFlex.forwardToIVRMenu}}"
+                ],
+                "type": "equal_to",
+                "value": "sales"
+              }
+            ]
+          },
+          {
+            "next": "sayServices",
+            "event": "match",
+            "conditions": [
+              {
+                "friendly_name": "services",
+                "arguments": [
+                  "{{widgets.sendToFlex.forwardToIVRMenu}}"
+                ],
+                "type": "equal_to",
+                "value": "services"
+              }
+            ]
+          },
+          {
+            "next": "mainMenu",
+            "event": "match",
+            "conditions": [
+              {
+                "friendly_name": "main",
+                "arguments": [
+                  "{{widgets.sendToFlex.forwardToIVRMenu}}"
+                ],
+                "type": "equal_to",
+                "value": "main"
+              }
+            ]
+          }
+        ],
+        "properties": {
+          "input": "{{widgets.sendToFlex.forwardToIVRMenu}}",
+          "offset": {
+            "x": 1270,
+            "y": 1460
+          }
+        }
+      }
+    ],
+    "initial_state": "Trigger",
+    "flags": {
+      "allow_concurrent_calls": true
+    }
+  }
